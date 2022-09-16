4dd91e8a4d195ec584f05d8fb633a870decf9b7b
Kevin Miller <kevin@kevee.net>
2022-09-16 14:54:19 -0700

    chore: Rename app file

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 import { initializeBlock } from '@airtable/blocks/ui'
 import React from 'react'
 
-import RandomRecordGeneratorApp from './app'
+import RandomRecordGeneratorApp from './random-name-generator'
 
 initializeBlock(() => <RandomRecordGeneratorApp />)
